320e5a0bd993697d05cbad9ebaf04399f102e334
Jan Popelka <popojan@protonmail.com>
2022-08-01 04:34:26 +0200

    feat: sum of theoretical vs real tests

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use num_prime::nt_funcs::{is_prime, primes, nth_prime};
 
 fn myreduce(n:usize, ap:&BigUint, a: &Vec<BigUint>) -> BigUint{
     let mut accum = ap.clone();
-    for p in a[1..n].iter().chain(vec![BigUint::from(2_u64)].iter()) {
+    for p in a[0..n].iter().chain(vec![BigUint::from(2_u64)].iter()) {
         while &accum > p && accum.clone().rem(p) == BigUint::zero() {
             accum = accum.div(p);
         }
@@ -18,19 +18,19 @@ fn myreduce(n:usize, ap:&BigUint, a: &Vec<BigUint>) -> BigUint{
     return accum;
 
 }
-fn bigprime(a: &Vec<BigUint>, i:usize,j:usize, b: &mut Vec<BigUint>) -> (usize, usize){
+fn bigprime(a: &Vec<BigUint>, i:usize,j:usize,k:usize, b: &mut Vec<BigUint>) -> (usize, usize){
     let mut last = &a[i];
     let zero= BigUint::zero();
     let one= BigUint::one();
     let two = BigUint::from(2_u64);
+    let mut accum = two.pow(k as u32);
     let mut digit = BigUint::from(1_u64);
-    let mut accum = two.pow(i as u32);
     //let mut it = 0_usize;
     let mut leading = true;
     let mut trailing_zeros = 0;
     let mut leading_zeros = 0;
     for  p in &a[i+1..j] {
-        let add = digit.clone().mul(p.sub(last).to_biguint().unwrap().div(two.clone()).rem(two.clone()));
+        let add = digit.clone().mul(one.clone()-p.sub(last).to_biguint().unwrap().div(two.clone()).rem(two.clone()));
         if add == zero {
             if leading {
                 leading_zeros += 1;
@@ -69,9 +69,14 @@ fn bigprime(a: &Vec<BigUint>, i:usize,j:usize, b: &mut Vec<BigUint>) -> (usize, 
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: {} <count> <lo> <hi> <kk>", args.get(0).unwrap());
+        return;
+    }
     let cnt = usize::from_str_radix(&args.get(1).unwrap_or(&"1".to_string()),10).unwrap();
     let lo = usize::from_str_radix(&args.get(2).unwrap_or(&"100".to_string()),10).unwrap();
     let hi = usize::from_str_radix(&args.get(3).unwrap_or(&"200".to_string()),10).unwrap();
+    let kk = usize::from_str_radix(&args.get(4).unwrap_or(&"1".to_string()),10).unwrap();
 
     let mut a = Vec::<BigUint>::new();
     for p in primes(nth_prime((hi+1) as u64)+1 as u64).iter() {
@@ -82,31 +87,43 @@ fn main() {
     let mut lastcnt = 0;
     let mut set = HashSet::<BigUint>::new();
     let mut _hi = hi;// + rng.gen::<usize>() % (hi-lo) as usize;
+    let mut numbers_tested = 0;
+    let mut numbers_tested_total = 0;
+    let mut numbers_tested_average = 0;
     while lastcnt < cnt {
         let mut  _lo = lo;//(hi as f64).sqrt().ceil() as usize;
         //_lo = rng.gen::<usize>() % _lo as usize;
-        let h =  (hi as f64).sqrt().sqrt().ceil() as usize;
+        //let h =  (hi as f64).sqrt().sqrt().ceil() as usize;
         _hi-=1;
-        if _hi < lo {
+        if _hi < lo || _hi > hi {
             break;
         }
         let mut skip_start = 0;
         let mut skip_end = 0;
         for i in _lo .. _hi {
-            if skip_start > 0 || i > _lo + h - skip_end {
+            if skip_start > 0 || i > _hi - skip_end {
                 skip_start -= 1;
                 continue;
             }
             //println!("{},{},{}",b.len(),i,_hi);
-            let (skipl, skipr) = bigprime(&a, i, _hi, &mut b);
+            let (skipl, skipr) = bigprime(&a, i, _hi,kk, &mut b);
             skip_end += skipr;
             skip_start += skipl;
+            numbers_tested += 1;
             if b.len() > 0 {
                 for p in b.iter() {
                     if !set.contains(p) {
-                        println!("{}",p);
+                        let binary_digits = p.to_str_radix(2).len();
+                        let decimal_digits = p.to_str_radix(10).len();
+                        let average_tests = binary_digits*f64::ln(2.0).ceil() as usize;
+                        println!("{}/{}\t{}\t{}\t{}\tbigprime({}, {}, {})",
+                                 numbers_tested, average_tests,
+                                 binary_digits, decimal_digits, p, i, _hi, kk);
                         set.insert(p.clone());
                         lastcnt += 1;
+                        numbers_tested_total += numbers_tested;
+                        numbers_tested_average += average_tests;
+                        numbers_tested = 0;
                         if lastcnt >= cnt {
                             break;
                         }
@@ -119,5 +136,6 @@ fn main() {
             }
         }
     }
+    println!("Total tests {} / average tests {}", numbers_tested_total, numbers_tested_average);
     return;
 }
